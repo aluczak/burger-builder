ac8ab1c87e158c19fa59ff03e109dbe1d7ca1368
Adam Luczak <aluczak@objectivity.co.uk>
2018-09-05 13:10:46 +0200

    Added DrawerToggle

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,10 +3,11 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
+        <DrawerToggle clicked={props.drawerToggleClicked} />
         <div className={classes.Logo}>
             <Logo />
         </div>
