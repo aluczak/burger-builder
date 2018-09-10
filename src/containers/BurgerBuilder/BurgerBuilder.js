34e88707061ce18b5a7249b1714cd35a7d61933d
Adam Luczak <aluczak@outlook.com>
2018-09-10 21:59:56 +0200

    Added contact data component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -89,28 +89,19 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         this.setState({ loading: true });
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     totalPrice: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Adam',
-        //         address: {
-        //             street: 'Graniczna',
-        //             zipCode: '54-610',
-        //             country: 'Poland'
-        //         },
-        //         email: 'dart@gmail.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // };
 
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({ loading: false, purchasing: false });
-        //     }).catch(error => {
-        //         this.setState({ loading: false, purchasing: false });
-        //     });
-        this.props.history.push('/checkout');
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        }
+
+        queryParams.push('price=' + this.state.totalPrice);
+
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
     }
 
     render() {
