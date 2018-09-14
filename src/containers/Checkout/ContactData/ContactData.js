31d4533765ce86769ec2e68016d2b1b6b2a3a7c8
Adam Luczak <aluczak@outlook.com>
2018-09-14 19:04:08 +0200

    Added purchase action creator

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -90,8 +90,7 @@ class ContactData extends Component {
                 touched: false
             }
         },
-        formIsValid: false,
-        loading: false
+        formIsValid: false
     }
 
     orderHandler = (event) => {
@@ -173,7 +172,7 @@ class ContactData extends Component {
             ))}
             <Button btnType='Success' disabled={!this.state.formIsValid}>Order</Button>
         </form>);
-        if(this.state.loading) {
+        if(this.props.loading) {
             form = <Spinner />;
         }
 
@@ -189,13 +188,14 @@ class ContactData extends Component {
 const mapStateToProps = state => {
     return {
         ingredients: state.ingredients,
-        totalPrice: state.totalPrice
+        totalPrice: state.totalPrice,
+        loading: state.loading
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
     }
 };
 
