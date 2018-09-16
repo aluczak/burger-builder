25ac1f3843e283165aec63125bf51b6b65689443
Adam Luczak <aluczak@outlook.com>
2018-09-16 16:33:57 +0200

    Moved checkValidity method to utility file

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,6 +8,7 @@ import Input from '../../../components/UI/Input/Input';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import classes from './ContactData.css';
 import * as actions from '../../../store/actions/index';
+import { checkValidity }  from '../../../store/utility';
 
 class ContactData extends Component {
     state = {
@@ -110,25 +111,6 @@ class ContactData extends Component {
         this.props.onOrderBurger(order, this.props.token);
     }
 
-    checkValidity(value, rules) {
-        let isValid = true;
-        if(rules) {
-            if(rules.required) {
-                isValid = value.trim() !== '' && isValid;
-            }
-    
-            if(rules.minLength) {
-                isValid = value.length >= rules.minLength && isValid;
-            }
-    
-            if(rules.maxLength) {
-                isValid = value.length <= rules.maxLength && isValid;
-            }
-        }
-
-        return isValid;
-    }
-
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedOrderForm = { 
             ...this.state.orderForm 
@@ -139,7 +121,7 @@ class ContactData extends Component {
         };
 
         updatedFormElement.value = event.target.value;
-        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        updatedFormElement.valid = checkValidity(updatedFormElement.value, updatedFormElement.validation);
         updatedFormElement.touched = true;
         updatedOrderForm[inputIdentifier] = updatedFormElement;
         let formIsValid = true;
