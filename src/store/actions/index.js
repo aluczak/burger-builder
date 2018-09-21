a8b154c3c0f47a527912fc8eff445117700832c0
Adam Luczak <aluczak@outlook.com>
2018-09-21 22:18:25 +0200

    Added redux-saga and rewritten authenticaton to it

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -15,5 +15,10 @@ export {
     auth,
     logout,
     setAuthRedirectPath,
-    authCheckState
+    authCheckState,
+    logoutSucceed,
+    authStart,
+    authSuccess,
+    authFailure,
+    checkAuthTimeout
 } from './auth';
